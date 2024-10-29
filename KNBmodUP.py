3cae002db1807a9f385d51b59a422719e2ec86be
deprome <ananasik_337@mail.ru>
2024-10-29 14:13:12 +0300

    minor update

--- KNBmodUP.py
+++ KNBmodUP.py
@@ -26,6 +26,9 @@ class Bot():
         self.probability_p = [0, 0, 0] # list for op probs 
         self.probability_c = [1 / 3, 1 / 3, 1 / 3] # list for op probs 
 
+        self.w_p = [1, 1, 1] # wigth probs
+        self.w_c = [1, 1, 1] # wigth chain
+
         self.count_op = [0, 0, 0] # count op moves
         self.count_me = [0, 0, 0] # count my moves
 
@@ -49,7 +52,7 @@ class Bot():
 
         if self.is_win(self.previousSelfChoice, previous_opponent_choice):
             self.wins_me[self.previousSelfChoice] += 1
-        else:
+        elif self.is_win(previous_opponent_choice, self.previousSelfChoice):
             self.wins_op[previous_opponent_choice] += 1
     
         self.lap += 1
@@ -65,11 +68,27 @@ class Bot():
         # res_p = (choices([0, 1, 2], weights=self.probability_p)[0] + 1) % 3
         # res_c = (choices([0, 1, 2], weights=self.probability_c)[0] + 1) % 3
 
-        res = (choices([0, 1, 2], weights=[(x + y) / 2 for x, y in zip(self.probability_p, self.probability_c)])[0] + 1) % 3
+        res = (choices([0, 1, 2], weights=[(x * x_w + y * y_w) / 2 for x, y, x_w, y_w in zip(self.probability_p, self.probability_c, self.w_p, self.w_c)])[0] + 1) % 3
         self.previousSelfChoice = res
         return self.req[res]
 
 
     def on_game_end(self) -> None:
-        # print(f"Процент побед: {sum(self.wins_me) / (sum(self.wins_op) + sum(self.wins_me)) * 100:.2f}%      {(self.wins_op)} {(self.wins_me)}")
+        print(f"Процент побед: {sum(self.wins_me) / (sum(self.wins_op) + sum(self.wins_me)) * 100:.2f}%      {(self.wins_op)} {(self.wins_me)}")
         pass
+
+
+if __name__ == "__main__":
+    raunds = 50
+    sets = 10
+
+    bot1 = Bot()
+    bot1.on_game_start()
+
+    for i in range (0, sets):
+        bot1.choose(52)
+
+        for j in range (0, raunds):
+            bot1.choose(randint(1, 3))
+
+        bot1.on_game_end()
